65980b649041ef269d38f76214f0f195c14c38e4
tqvuong97 <quocvuong.tqv97@gmail.com>
2020-02-17 23:48:53 +0700

    commit homepage 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
-
+  config.assets.js_compressor = :uglifier
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
